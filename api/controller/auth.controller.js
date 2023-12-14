1930462b1e9d4c29c3f40b3a5113faf70510788b
Prathak Gupta <Prathakgupta280720@gmail.com>
2023-12-14 19:09:31 +0530

    created user model and signup api route

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+    const { username , email, password} = req.body;
+    const hashedpassword =bcryptjs.hashSync(password ,10);
+    const newUser = new User({username, email,password : hashedpassword});
+    try {
+        await newUser.save();
+        res.status(201).json("done");
+    } catch(err){
+        if (err.code === 11000 && err.keyPattern && err.keyPattern.username) {
+            // The error code 11000 indicates a duplicate key (unique constraint) violation
+            res.status(400).json({ error: "Username already exists" });
+        }
+        else if (err.code === 11000 && err.keyPattern && err.keyPattern.email) {
+            // The error code 11000 indicates a duplicate key (unique constraint) violation
+            res.status(400).json({ error: "Email already exists" });
+        } else {
+            // Handle other errors
+            res.status(500).json({ error: err.message || "Internal Server Error" });
+        }
+    }
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import authRouter from "../api/routes/auth.route.js"
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/MERN_REAL_ESTATE").then(() => {
     console.log("connected");
@@ -12,3 +14,5 @@ mongoose.connect("mongodb://localhost:27017/MERN_REAL_ESTATE").then(() => {
 app.listen(3000, ()=>{
     console.log("listening on port 3000");
 });
+
+app.use("/api/auth", authRouter);
