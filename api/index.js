5b6c6042322e9ab6afcdb59cae417ebd6c19f8ff
Prathak Gupta <Prathakgupta280720@gmail.com>
2023-12-14 17:02:03 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log("listening on port 3000");
+mongoose.connect("mongodb://localhost:27017/MERN_REAL_ESTATE").then(() => {
+    console.log("connected");
+}).catch((err) => {
+    console.log(err);
 })
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000");
+});
